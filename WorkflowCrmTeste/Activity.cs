a7873750c99b47e549f2bec2ae99515fa900ea8b
brunoksato <bruno.sato@live.com>
2013-11-25 17:50:38 -0200

    Create workflow crm

---
+++ WorkflowCrmTeste/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System.Activities;
+using System;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace WorkflowCrmTeste
+{
+
+    public sealed class Activity : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            this.NewGuid.Set(context, Guid.NewGuid().ToString());
+        }
+
+        [Output("NewGuid")]
+        public OutArgument NewGuid { get; set; }
+    }
+}
